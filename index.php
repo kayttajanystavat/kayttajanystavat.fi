11c5322f781aaaa81a8ca734cb99bb0fdabd2c69
Akseli Nurmio <akseli@nurmio.fi>
2018-01-14 20:16:54 +0200

    Replace old Analytics with the new one

--- index.php
+++ index.php
@@ -78,15 +78,13 @@ $sivun_title = array("etusivu" => "- Etusivu",
       </div> <!-- kaikki -->
     </div> <!-- taustavari -->
   <div id="taustavari-ala"></div>
-  <script type="text/javascript">
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-25774316-1']);
-    _gaq.push(['_trackPageview']);
-    (function() {
-      var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-      ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-112417705-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-112417705-1');
   </script>
   </body>
   <?php
